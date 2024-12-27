1a15c7cc205d8dd2ae5422434f3716508c319b18
Imran Feroz <imran.feroz@zekiexperts.com>
2024-12-27 12:29:42 +0500

    leads assignment implementation

--- Core/Interfaces/IServices/ILeadsService.cs
+++ Core/Interfaces/IServices/ILeadsService.cs
@@ -4,10 +4,10 @@ namespace ParadisePromotions.Core.Interfaces.IServices
 {
     public interface ILeadsService
     {
-        Task<bool> CreateLeads(Lead pars);
+        Task<bool> CreateLeads(Lead lead);
         Task<IEnumerable<Lead>> GetAllLeads();
         Task<Lead> GetLeadsById(int id);
-        Task<bool> UpdateLeads(Lead pars);
+        Task<bool> UpdateLeads(IEnumerable<Lead> lead);
         Task<bool> DeleteLeads(int id);
         Task<IEnumerable<Lead>> GetLeadsCount(LeadsFilter filter);
     }
