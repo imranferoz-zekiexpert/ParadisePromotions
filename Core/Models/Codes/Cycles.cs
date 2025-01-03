ca17f5535b898ace1cd3900027dd4cc1981e0a02
Imran Feroz <imran.feroz@zekiexperts.com>
2025-01-03 23:11:25 +0500

    cycle model updated

--- Core/Models/Codes/Cycles.cs
+++ Core/Models/Codes/Cycles.cs
@@ -8,7 +8,7 @@ namespace ParadisePromotions.Core.Models.Codes
     {
         public int? ID { get; set; }
         public string? Cycle { get; set; }  // 'Cycle' might be a reserved keyword, so naming it CycleName to avoid conflicts.
-        public int? StaffId { get; set; }
+        public string? StaffId { get; set; }
         public DateTime? CreatedDate { get; set; }
         public int? CreatedBy { get; set; }
         public DateTime? UpdatedDate { get; set; }
